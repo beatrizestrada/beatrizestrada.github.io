ec708ca0edeabcf5239bc4ff618b7a3e003b96a2
mnorte <martimn1998@gmail.com>
2025-07-21 20:25:41 +0100

    change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: require("tailwindcss/colors").indigo,
+        primary: {
+          50: "#f3eef9",
+          100: "#eae0f4",
+          200: "#d9c7eb",
+          300: "#ccb4e3",
+          400: "#a87fcd",
+          500: "#9263ba",
+          600: "#7c4fa1",
+          700: "#694485",
+          800: "#56386b",
+          900: "#381f4c",
+          950: "#1c0f26",
+        },
       },
     },
   },
